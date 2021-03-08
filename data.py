329a18689a4c314d2a9b589bde65d20b16962620
Laveezah Noor <laveezahnoor@gmail.com>
2021-03-08 15:07:55 +0500

    Commit

--- data.py
+++ data.py
@@ -1,17 +1,18 @@
-from openpyxl import load_workbook, Workbook
-
-# workbook = load_workbook(filename="data.xlsx")
-workbook = Workbook()
+from openpyxl import load_workbook
+# from openpyxl import Workbook
+# wb = Workbook()
+workbook = load_workbook(filename="data.xlsx")
+# workbook = Workbook()
 
 sheet = workbook.active
-
+sheet1 = workbook['Users']
 # sheet["A1"] = "hello"
 # sheet["B1"] = "world!"
-sheet.title = 'Users'
+# sheet.title =
 # products_sheet = workbook["Products"]
 
-workbook.save(filename="data.xlsx")
+# workbook.save(filename="data.xlsx")
 # print(workbook.sheetnames)
 # print(workbook.active)
 
-print(sheet.title)
+print(workbook.active.title)
